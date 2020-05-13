a5fc8a0b1ca77e1a9a5cdf082f692d074a5eecdb
Ahnming Loke <aloke@marvel.com>
2020-05-13 14:49:52 -0400

    feat: begin to determine order of elements based on mouse position

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ draggables.forEach(draggable => {
 });
 
 containers.forEach(container => {
-  container.addEventListener('dragover', () => {
-    console.log('drag over');
+  container.addEventListener('dragover', event => {
+    /* remove circle-x (do not) symbol when dragging, as dropping inside an element is inheritantly disabled */
+    event.preventDefault();
+
+    /*  */
+    const nextDraggableSibiling = getDraggablePosition(container, event.clientY);
+
+    /* append element to the end of the target container */
+    const draggableElement = document.querySelector('.dragging');
+    container.appendChild(draggableElement);
   })
 });
+
+function getDraggablePosition( container, yCoord ) {
+  /* convert this to array */
+  const draggableElementList = [...container.querySelectorAll('.container__draggable:not(.dragging')];
+
+  draggableElementList.reduce( (closestSubsequentElement, child) => {
+    const test = child.getBoundingClientRect();
+    console.log('getDraggablePosition, boundingBox: ', test)
+  }, {offset: Number.POSITIVE_INFINITY})
+}
