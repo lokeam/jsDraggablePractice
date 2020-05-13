be3742de798b7873bb50fed66dc9adfefa3a1800
Ahnming Loke <aloke@marvel.com>
2020-05-13 15:15:48 -0400

    feat: determined mouse position and insert element accordingly

--- script.js
+++ script.js
@@ -18,12 +18,18 @@ containers.forEach(container => {
     /* remove circle-x (do not) symbol when dragging, as dropping inside an element is inheritantly disabled */
     event.preventDefault();
 
-    /*  */
     const nextDraggableSibiling = getDraggablePosition(container, event.clientY);
 
     /* append element to the end of the target container */
     const draggableElement = document.querySelector('.dragging');
-    container.appendChild(draggableElement);
+    console.log('nextDraggableSibiling: ', nextDraggableSibiling);
+
+    if (nextDraggableSibiling == null) {
+      container.appendChild(draggableElement);
+    } else {
+      container.insertBefore(draggableElement, nextDraggableSibiling);
+    }
+
   })
 });
 
@@ -31,8 +37,24 @@ function getDraggablePosition( container, yCoord ) {
   /* convert this to array */
   const draggableElementList = [...container.querySelectorAll('.container__draggable:not(.dragging')];
 
-  draggableElementList.reduce( (closestSubsequentElement, child) => {
-    const test = child.getBoundingClientRect();
-    console.log('getDraggablePosition, boundingBox: ', test)
-  }, {offset: Number.POSITIVE_INFINITY})
+  return draggableElementList.reduce( (closestSubsequentElement, child) => {
+    const boundingBox = child.getBoundingClientRect();
+    
+    /* distance between center of box and mouse cursor */
+    const offset = yCoord - boundingBox.top - boundingBox.height / 2;
+
+    console.log('boundingBoxOffset: ', offset);
+    /* we're only concerned with offsets < 0 (hovering over element instead of below) but as close to 0 as possible */
+    console.log('offset: ', offset);
+    if ( offset < 0 && offset > closestSubsequentElement.offset ) {
+      console.log('getDraggablePosition, if conditional, about to return');
+      return {
+        offset: offset,
+        element: child
+      }
+    } else {
+      console.log('getDraggablePosition, else conditional');
+      return closestSubsequentElement;
+    }
+  }, {offset: Number.NEGATIVE_INFINITY}).element
 }
